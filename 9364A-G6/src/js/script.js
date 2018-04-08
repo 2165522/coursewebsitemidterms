44027ef03db22671983c5a6a91d9574319a1b3a9
vince <enteng@example.com>
2018-04-08 23:27:41 +0800

    edit src

--- 9364A-G6/src/js/script.js
+++ 9364A-G6/src/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover();   
 });
+
+function toggle_visibility(id) {
+                var e = document.getElementById(id);
+                if(e.style.display == 'block')
+                    e.style.display = 'none';
+                else{
+                    e.style.display = 'block';
+                }
+                    
+            }
